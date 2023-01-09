2113178a9bc38eb50ae0ae3dcf164e58750bc90f
Admin <Admin@DESKTOP-N2E6CKC>
2023-01-09 16:27:07 +0530

    my commit

---
+++ Demo1/src/main/java/DemoJenkin/Test1.java
@@ -0,0 +1,13 @@
+package DemoJenkin;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void m1() {
+		Reporter.log("Hello",true);
+		
+	}
+
+}
---
+++ Demo1/src/main/java/DemoJenkin/Test2.java
@@ -0,0 +1,12 @@
+package DemoJenkin;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void m2() {
+		Reporter.log("Hii",true);
+	}
+
+}
